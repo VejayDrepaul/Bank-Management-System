30c38d62ef8167ed04e6b5b5bc566cbc83f4120f
Vejay Drepaul <dvejay@outlook.com>
2021-02-25 13:56:19 -0500

    Welcome function

--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+void welcomeScreen();
+
 int main() 
+{
+    welcomeScreen(); 	
+    return 0;
+}
+
+void welcomeScreen()
 {
     cout<<"\n\n\n\t   BANK";
     cout<<"\n\n\tMANAGEMENT";
     cout<<"\n\n\t  SYSTEM" << endl;
-    
-    return 0;
 }
+ 
