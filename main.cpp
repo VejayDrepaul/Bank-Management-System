878564a210b23fc147044523d1dfcf6324e3a12b
Vejay Drepaul <dvejay@outlook.com>
2021-02-25 13:04:48 -0500

    using...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::endl;
+using std::cin;
 
 int main() 
 {
+    cout<<"\n\n\n\t   BANK";
+    cout<<"\n\n\tMANAGEMENT";
+    cout<<"\n\n\t  SYSTEM" << endl;
+    
     return 0;
 }
