19a2dff73df6194f7fe94d324a92903366ab55b6
Vejay Drepaul <dvejay@outlook.com>
2021-03-09 13:09:49 -0500

    admin file

---
+++ administrator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+void adminCreation()
+{
+	string master_pass;
+	cout << "Enter a master password";
+	cin >> master_pass;
+}
