df13ce3954f7ec31f5f42fe24c90817c04cb8635
Vejay Drepaul <dvejay@outlook.com>
2021-03-11 09:22:40 -0500

    Main file

--- main.cpp
+++ main.cpp
@@ -1,55 +1,59 @@
 #include <iostream>
 #include <iomanip>
-#include <string>   
+#include <string>
 #include <fstream>
 #include "includes/NewAccount.h"
-#include "includes/administrator.h"
+#include "includes/Administrator.h"
 
 using std::cout;
 using std::endl;
 using std::cin;
 
 void welcomeScreen();
+void option_menu();
 
-int main() 
+int main()
 {
-    welcomeScreen();
-   
-    cout<<"\n\n\n\tMAIN MENU";
-    cout<<"\n\n\t01. NEW ACCOUNT";
-    cout<<"\n\n\t02. DEPOSIT AMOUNT";
-    cout<<"\n\n\t03. WITHDRAW AMOUNT";
-    cout<<"\n\n\t04. BALANCE ENQUIRY";
-    cout<<"\n\n\t05. ALL ACCOUNT HOLDER LIST";
-    cout<<"\n\n\t06. CLOSE AN ACCOUNT";
-    cout<<"\n\n\t07. MODIFY AN ACCOUNT";
-    cout<<"\n\n\t08. EXIT";
-    cout<<"\n\n\tSelect Your Option (1-8) ";
-    cout << "\n\n";
-    
-    int options;
-    
-    cout << "What do you want to do: ";
-    cin >> options;
-    
-    switch(options) {
-        case 1:
-            new_account();
-        case 5:
-	    system("python3 depositor.py");
-            system("cd csv && cat AccountInfo.csv");
-        case 8:
-            exit(0);
-    }
-    
-    return 0;
+  welcomeScreen();
+  option_menu();
+  return 0;
 }
 
 void welcomeScreen()
 {
-    cout<<"\n\n\n\t   BANK";
-    cout<<"\n\n\tMANAGEMENT";
-    cout<<"\n\n\t  SYSTEM" << endl;
-    
-    cout << "\n";
-}       
+  cout<<"\n\n\n\t   BANK";
+  cout<<"\n\n\tMANAGEMENT";
+  cout<<"\n\n\t  SYSTEM" << endl;
+
+  cout << "\n";
+}
+
+void option_menu()
+{
+  cout<<"\n\n\n\tMAIN MENU";
+  cout<<"\n\n\t01. NEW ACCOUNT";
+  cout<<"\n\n\t02. DEPOSIT AMOUNT";
+  cout<<"\n\n\t03. WITHDRAW AMOUNT";
+  cout<<"\n\n\t04. BALANCE ENQUIRY";
+  cout<<"\n\n\t05. ALL ACCOUNT HOLDER LIST";
+  cout<<"\n\n\t06. CLOSE AN ACCOUNT";
+  cout<<"\n\n\t07. MODIFY AN ACCOUNT";
+  cout<<"\n\n\t08. EXIT";
+  cout<<"\n\n\tSelect Your Option (1-8) ";
+  cout << "\n\n";
+
+  int options;
+
+  cout << "What do you want to do: ";
+  cin >> options;
+
+  switch(options) {
+    case 1:
+      new_account();
+    case 5:
+      system("python3 depositor.py");
+      system("cd csv && cat AccountInfo.csv");
+    case 8:
+      exit(0);
+  }
+}
