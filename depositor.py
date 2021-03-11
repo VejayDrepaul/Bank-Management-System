6e80b696e05b63d478307e9928bbc1e30685e297
Vejay Drepaul <dvejay@outlook.com>
2021-03-10 20:24:11 -0500

    depositor file

--- depositor.py
+++ depositor.py
@@ -1,11 +1,16 @@
 import csv
 
-f = open('csv/AccountInfo.csv', 'r')
-reader = csv.reader(f)
-mylist = list(reader)
-f.close()
-mylist[2][3] = " $30000"
-mynewlist = open('csv/AccountInfo.csv', 'w', newline = '')
-csvwrite = csv.writer(mynewlist)
-csvwrite.writerows(mylist)
-mynewlist.close()
+
+o = input("What is the name: ")
+if o == "Vejay":
+    f = open('csv/AccountInfo.csv', 'r')
+    reader = csv.reader(f)
+    mylist = list(reader)
+    f.close()
+    mylist[2][3] = " $30000"
+    mynewlist = open('csv/AccountInfo.csv', 'w', newline = '')
+    csvwrite = csv.writer(mynewlist)
+    csvwrite.writerows(mylist)
+    mynewlist.close()
+else:
+    print("Failure 404")
